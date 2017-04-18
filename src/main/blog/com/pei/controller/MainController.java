4680ea297a91928bcd9ddeed410327a8de5e17b6
tianyaqpzm <tianyaqpzm@163.com>
2017-04-18 10:12:39 +0800

    first use intellij to github

---
+++ src/main/blog/com/pei/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.pei.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by pei on 2017/4/18.
+ */
+
+@Controller
+public class MainController {
+    /**
+     * （1）@Controller注解：采用注解的方式，可以明确地定义该类为处理请求的Controller类；
+     * （2）@RequestMapping()注解：用于定义一个请求映射，value为请求的url，值为 / 说明，该请求首页请求，method用以指定该请求类型，一般为get和post；
+     * （3）return "index"：处理完该请求后返回的页面，此请求返回 index.jsp页面。
+     * @return
+     */
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public  String index(){
+        return "index";
+    }
+}
